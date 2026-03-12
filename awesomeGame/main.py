117f6af750bc8fa13f4376afb497f5979569bb02
Victor Jaramillo <victorajm@outlook.com>
2026-03-11 20:27:39 -0600

    add command handling and help functionality to main loop

--- awesomeGame/main.py
+++ awesomeGame/main.py
@@ -2,6 +2,17 @@
 
 from utils import parse_command
 
+def show_help():
+  print("exit    - Quit the game\nhelp    - Show this help message\n")
+
+def handle_command(command, args):
+  if command is None:
+    pass
+  elif command == "help":
+    show_help()
+  else:
+    print("Unknown command. Type 'help' for a list of commands.")
+
 def main():
 
   print("System online.\n")
@@ -9,9 +20,11 @@ def main():
   while True:
     prompt = input("> ")
     command, args = parse_command(prompt)
-    print(command, args)
+
     if command == "exit":
       break
+
+    handle_command(command, args)
 
   print("Connection terminated.")
 
